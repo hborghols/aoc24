d205ffad54e1c8b62f2fa6dce428fe9c16fd5bb5
Henk Borghols <henk@myonlinestore.com>
2024-12-01 16:59:31 +0100

    Day 1

---
+++ day1/day1.py
@@ -0,0 +1,35 @@
+with open ('input.txt') as f:
+    data = f.read().splitlines()
+
+listOne = []
+listTwo = []
+
+for line in data:
+    splitLine = (line.split('   '))
+    listOne.append(splitLine[0])
+    listTwo.append(splitLine[1])
+    
+def part1():
+    sortedOne = sorted(listOne)
+    sortedTwo = sorted(listTwo)
+    distance = 0
+
+    for i in range(len(listOne)):
+        distance += abs(int(sortedTwo[i]) - int(sortedOne[i]))
+
+    print(distance)
+
+def part2():
+    appearances = {}
+    similarity = 0
+
+    for i in range(len(listTwo)):
+        appearances[listTwo[i]] = appearances.get(listTwo[i], 0) + 1
+
+    for i in range(len(listOne)):
+        similarity = int(listOne[i]) * appearances.get(listOne[i], 0)
+    
+    print(similarity)
+
+part1()
+part2()
---
+++ test.py
@@ -0,0 +1,6 @@
+print('Hello world');
+
+words = ['this', 'is', 'a', 'test'];
+
+for w in words:
+	print(w + '\n');
